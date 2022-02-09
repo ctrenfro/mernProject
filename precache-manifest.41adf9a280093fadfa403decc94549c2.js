90829f7f5daa331cc659191a2b53ebaa5f3c4ae9
ctrenfro <63364677+ctrenfro@users.noreply.github.com>
2022-02-09 16:48:30 -0500

    Updates

---
+++ precache-manifest.41adf9a280093fadfa403decc94549c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78508fa066b8cd304f429447c68c6c93",
+    "url": "/mernProject/index.html"
+  },
+  {
+    "revision": "ec53c5d33e2e42e9a4fc",
+    "url": "/mernProject/static/css/main.1f693d8d.chunk.css"
+  },
+  {
+    "revision": "56d12950484e04f35b85",
+    "url": "/mernProject/static/js/2.786e6dc1.chunk.js"
+  },
+  {
+    "revision": "ec53c5d33e2e42e9a4fc",
+    "url": "/mernProject/static/js/main.efe865d9.chunk.js"
+  },
+  {
+    "revision": "01408a21c6efb8383756",
+    "url": "/mernProject/static/js/runtime-main.f76a3290.js"
+  }
+]);
